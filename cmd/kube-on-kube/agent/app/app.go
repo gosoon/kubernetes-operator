06871a1e422fa2d8d92aaffe8a3d7a232cb552b3
gosoon <tianfeiyu0@gmail.com>
2019-09-19 09:19:31 +0800

    feat: add installer

--- cmd/kube-on-kube/agent/app/app.go
+++ cmd/kube-on-kube/agent/app/app.go
@@ -1,15 +1,26 @@
 package app
 
 import (
+	"net"
 	"time"
 
 	"github.com/gosoon/glog"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 
 	"github.com/gosoon/kubernetes-operator/pkg/cluster"
 	"github.com/gosoon/kubernetes-operator/pkg/cluster/create"
 )
+
+type flagpole struct {
+	Name      string
+	Config    string
+	ImageName string
+	Retain    bool
+	Wait      time.Duration
+	Port      int
+}
 
 // NewServerCommand returns a new cobra.Command for kube-on-kube server
 func NewServerCommand() *cobra.Command {
@@ -26,10 +37,21 @@ func NewServerCommand() *cobra.Command {
 	cmd.Flags().StringVar(&flags.Config, "config", "", "path to a kind config file")
 	cmd.Flags().StringVar(&flags.ImageName, "image", "", "node docker image to use for booting the cluster")
 	cmd.Flags().DurationVar(&flags.Wait, "wait", time.Duration(0), "Wait for control plane node to be ready (default 0s)")
+	cmd.Flags().DurationVar(&flags.Port, "port", 10023, "installer agent grpc server port(default 10023)")
 	return cmd
 }
 
+type Server struct{}
+
 func run(flags *flagpole) {
+	l, err := net.Listen("tcp", PORT)
+	if err != nil {
+		glog.Fatalf("failed to listen: %v", err)
+	}
+	server := grpc.NewServer()
+	// register grpc server
+	pb.RegisterScpServer(server, &Server{})
+	glog.Fatal(server.Serve(l))
 
 	// TODO :receiver operator ecs config
 
